7f00bf16b38a5614c826cec6e9557a656aafc6dd
Mahamzafar768 <157698147+MSulemanBhanbhro@users.noreply.github.com>
2025-01-22 00:13:37 +0500

    updated version of Avion

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "cdn.sanity.io",
+          port: "",
+          pathname: "/images/**",
+        },
+        {
+          protocol: "https",
+          hostname: "www.google.com",
+          port: "",
+          pathname: "/maps/embed/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
